d9d7e70a8268958e0d3164708516de5cbab5b6cb
RamonReilman <gmodlovver@gmail.com>
2026-01-22 12:18:23 +0100

    adds pydoc and reformats code to be clean

--- classes/CoxProportionalHazard.py
+++ classes/CoxProportionalHazard.py
@@ -1,13 +1,20 @@
-from typing import Any
+"""
+Implementation of Cox Proportional Hazard Model
+Author: Ramon Reilman, bfv3
+Supported by: Hanze
+"""
 
+import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
 import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import xlabel
 
 
 class CoxProportionalHazard:
+    """
+    Cox Proportional Hazard Model
+    A wikipedia-supported implementation of the Cox Proportional hazard model
+    """
 
     def __init__(self):
         self._df = None
@@ -17,6 +24,16 @@ class CoxProportionalHazard:
         self._result = pl.DataFrame()
 
     def fit(self, df, time_col, event_col):
+        """
+        Optimizes beta values for all covariates using the breslow-method.
+        The b-values get updated using the Newton-Raphson algorithm.
+
+        :param df: Polars df containing time, censored data, and covariates
+        :param time_col: string containing name of the column that holds time
+        :param event_col: string containing name of the column that holds the events
+        :return: polars df containing the beta value and hazard ratio for all covariates
+        """
+
         df = df.sort(time_col)
         self._df = df
         self._wanted_cov = df.drop([time_col, event_col])
@@ -25,13 +42,13 @@ class CoxProportionalHazard:
 
             score_list = []
             diff_list = []
-            for row_full, row_cov in zip(df.iter_rows(named = True), self._wanted_cov.iter_rows()):
+            for row_full, row_cov in zip(df.iter_rows(named=True), self._wanted_cov.iter_rows()):
                 if row_full[event_col] == 1:
                     time = row_full[time_col]
                     risk_set = df.filter(pl.col(time_col) >= time)
                     covariates_risk_set = risk_set.drop([time_col, event_col]).to_numpy().T
                     current_val = np.array(row_cov)
-                    relative_risk = self.calculate_relative_risk(covariates_risk_set)
+                    relative_risk = self.__calculate_relative_risk(covariates_risk_set)
                     score_list.append(self.__score_normal_CPHM(current_val, relative_risk, covariates_risk_set))
                     diff_list.append(self.__hessian_matrix_normal(relative_risk, covariates_risk_set.T))
             h_total = sum(diff_list)
@@ -63,27 +80,32 @@ class CoxProportionalHazard:
             print("No model has been fit, yet!")
 
     def plot_beta_value(self):
+        """
+        Creates a barplot to display the beta values
+        :return:
+        """
         temp_data = self._result.sort("beta values")
         ax = sns.barplot(x=temp_data["covariate"], y=np.round(temp_data["beta values"], 2), width=0.75,
                          saturation=1)
-        ax.set(ylabel = "beta values", title = "Beta values for all covariates")
+        ax.set(ylabel="beta values", title="Beta values for all covariates")
         self.__plot(ax)
 
     def plot_hazard_ratio(self):
+        """
+        Creates a barplot to display the hazard ratio
+        :return:
+        """
         temp_data = self._result.sort("beta values")
         ax = sns.barplot(x=temp_data["covariate"], y=np.round(temp_data["exp(beta)"], 2), width=0.75, saturation=1)
-        ax.set(ylabel = "hazard ratio, exp(beta value)", title = "Hazard ratio for all covariates")
+        ax.set(ylabel="hazard ratio, exp(beta value)", title="Hazard ratio for all covariates")
         self.__plot(ax)
 
-
-
-
-    def calculate_relative_risk(self,  risk_set) -> Any:
-        relative_risk =  np.exp(risk_set.T @ self._beta_values)
+    def __calculate_relative_risk(self, risk_set):
+        relative_risk = np.exp(risk_set.T @ self._beta_values)
         return relative_risk
 
     def __score_normal_CPHM(self, current_covariate, relative_risk, covariate):
-        sum_relative_time_cov = np.sum(relative_risk * covariate, axis = 1)
+        sum_relative_time_cov = np.sum(relative_risk * covariate, axis=1)
 
         return current_covariate - sum_relative_time_cov / sum(relative_risk)
 
@@ -93,6 +115,6 @@ class CoxProportionalHazard:
 
         weighted_cov = covariates.T @ (relative_risk[:, None] * covariates)
 
-        weighted_sum = (relative_risk[:, None] * covariates).sum(axis = 0)
+        weighted_sum = (relative_risk[:, None] * covariates).sum(axis=0)
         hessian = weighted_cov / rr_sum - np.outer(weighted_sum, weighted_sum) / (rr_sum ** 2)
         return hessian
